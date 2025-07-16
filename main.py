3e6290ed8a7a7ff3ccd62dbeb4c649308d04614e
Sachin Panayil <sachinpanayil01@gmail.com>
2025-07-16 16:40:20 -0400

    changed env syntax
    Signed-off-by: Sachin Panayil <sachinpanayil01@gmail.com>

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import asyncio
 from notion_action_extractor import NotionActionExtractor
 
 async def main():
-    NOTION_TOKEN = os.environ("NOTION_API_KEY")
-    KANBAN_DB_ID = os.environ("KANBAN_DB_ID")
-    ACTION_ITEMS_DB_ID = os.environ("ACTION_ITEMS_DB_ID")
+    NOTION_TOKEN = os.environ["NOTION_API_KEY"]
+    KANBAN_DB_ID = os.environ["KANBAN_DB_ID"]
+    ACTION_ITEMS_DB_ID = os.environ["ACTION_ITEMS_DB_ID"]
 
     extractor = NotionActionExtractor(NOTION_TOKEN, KANBAN_DB_ID, ACTION_ITEMS_DB_ID)
 
